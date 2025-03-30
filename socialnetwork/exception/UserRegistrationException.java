67300c468a0b8253bbef58815f0ae1c3f4139ed2
st1v4n <149948320+st1v4n@users.noreply.github.com>
2025-03-30 20:23:27 +0300

    Add files via upload

--- socialnetwork/exception/UserRegistrationException.java
+++ socialnetwork/exception/UserRegistrationException.java
@@ -1,1 +1,7 @@
+package socialnetwork.exception;
 
+public class UserRegistrationException extends Exception{
+    public UserRegistrationException(String content){
+        super(content);
+    }
+}
